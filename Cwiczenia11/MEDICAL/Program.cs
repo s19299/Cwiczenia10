b89e22421176fb400fbf79004c701d9250d9c77f
s19299 <62510703+s19299@users.noreply.github.com>
2020-05-25 14:31:26 +0200

    porawiony program z seedem

--- Cwiczenia11/MEDICAL/Program.cs
+++ Cwiczenia11/MEDICAL/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Wyklad10Sample.Models;
 
 namespace MEDICAL
 {
@@ -14,10 +15,57 @@ namespace MEDICAL
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+
+        void seed()
+        {
+            
+            var db = new s19299DbContext();
+
+            var p1 = new Prescription()
+            {
+                Date = DateTime.Now,
+                DueDate = DateTime.Now,
+                IdPatient = 2
+            };
+            var set1 = new HashSet<Prescription> { p1 };
+
+            var d1 = new Doctor
+            {
+                FirstName ="Kamil",
+                LastName ="Bednarski",
+                Email ="bednar@wp.pl",
+                Prescription = set1
+            };
+
+            var p2 = new Prescription()
+            {
+                Date = DateTime.Today,
+                DueDate = DateTime.Now,
+                IdPatient = 3,
+            };
+            
+            var set2 = new HashSet<Prescription>{p2};
+
+            var d2 = new Doctor
+            {
+                FirstName = "Herbert",
+                LastName = "Daca",
+                Email = "drdaca@wp.pl",
+                Prescription = set2
+            };
+
+            db.Doctor.Add(d2);
+            db.Doctor.Add(d1);
+
+            db.SaveChanges();
+        }
     }
 }
